c2363c3b8d0ba1249642a4bfa2d31ebb7e2eb09a
sahin <oluts@itu.edu.tr>
2018-02-25 21:53:14 +0300

    Tail recursive version of sundays1 is added

--- src/Hw1.hs
+++ src/Hw1.hs
@@ -1,5 +1,5 @@
 module Hw1
-    (dayOfWeek, sundays1, sundays2, leap, isIn, daysInMonth)
+    (dayOfWeek, sundays1, sundays2, sundays1_tail)
     where
 
 dayOfWeek :: (Integral a) => a -> a -> a -> a
@@ -11,7 +11,7 @@ dayOfWeek y m d = z
                     t1 = floor $ fromIntegral (13 * (m' + 1)) / 5.0
                     j = div y 100
                     k = mod y 100
-                    m' =  if m<=2 then m +12 else m 
+                    m' = if m <=2 then m +12 else m 
 
 sundays1 :: Integer -> Integer -> Integer
 sundays1 start end = sundays' start 1
@@ -26,7 +26,19 @@ sundays1 start end = sundays' start 1
                             rest = sundays' nextY nextM
                     
                     
-    
+sundays1_tail :: Integer -> Integer -> Integer
+sundays1_tail start end = sundays' start 1 0
+                    where
+                        sundays' :: Integer -> Integer -> Integer -> Integer
+                        sundays' y m acc
+                            | y > end = acc
+                            | dayOfWeek y m 1 == 1 = sundays' nextY nextM $ (+) acc 1
+                            | otherwise  = sundays' nextY nextM acc
+                            where
+                                nextY = if m == 12 then y+1 else y
+                                nextM = if m == 12 then 1 else m+1
+
+
 leap :: Integer -> Bool
 leap y = (mod y 4) == 0 && (mod y 100) /= 0 || (mod y 400) == 0
 
